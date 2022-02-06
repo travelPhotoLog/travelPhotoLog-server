f01b68a9d5506caebafc7fd3ebf6e835bd438c52
seonghoon-Oh <su5785@naver.com>
2022-02-06 18:20:46 +0900

    docs: Change file name & Add constant @constants.js

--- constants.js
+++ constants.js
@@ -1,6 +1,7 @@
 const ERROR_MESSAGE = Object.freeze({
   NOT_FOUNT: "Not Found",
   SERVER_ERROR: "Internal Server Error",
+  DUPLICATE_KEY_ERROR: "해당 닉네임이 존재합니다",
 });
 
 module.exports = ERROR_MESSAGE;
