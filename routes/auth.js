0b00caa7b08ef33b5b02fa3928792c22272afa19
seonghoon-Oh <su5785@naver.com>
2022-02-06 18:45:22 +0900

    feat: add controller for auth router

--- routes/auth.js
+++ routes/auth.js
@@ -1,53 +1,17 @@
 const express = require("express");
 
-const ERROR_MESSAGE = require("../constant");
-const User = require("../models/User");
-const checkToken = require("./middleware/tokenValidation");
-const checkUser = require("./middleware/userValidation");
+const checkToken = require("../middleware/tokenValidation");
+const checkUser = require("../middleware/userValidation");
+const {
+  postLogin,
+  postLogout,
+  postSignUp,
+} = require("../controllers/auth.controller");
 
 const router = express.Router();
 
-router.post("/login", checkUser, checkToken, async (req, res, next) => {
-  const { email } = req.body;
-
-  try {
-    const user = await User.findOne({ email }).exec();
-
-    res.json({
-      user: {
-        id: user._id,
-        email: user.email,
-        nickname: user.nickname,
-      },
-    });
-  } catch {
-    res.json({
-      error: {
-        message: ERROR_MESSAGE.SERVER_ERROR,
-        code: 500,
-      },
-    });
-  }
-});
-
-router.post("/logout", async (req, res, next) => {
-  const { email } = req.body;
-
-  try {
-    await User.findOneAndUpdate({ email }, { refreshToken: "" }).exec();
-
-    res.clearCookie("accessToken");
-    res.json({
-      result: "ok",
-    });
-  } catch {
-    res.json({
-      error: {
-        message: ERROR_MESSAGE.SERVER_ERROR,
-        code: 500,
-      },
-    });
-  }
-});
+router.post("/login", checkUser, checkToken, postLogin);
+router.post("/logout", postLogout);
+router.post("/sign-up", postSignUp);
 
 module.exports = router;
