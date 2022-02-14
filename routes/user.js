8225fb1b36523dcbff974cec0d1fbe727dbc87d6
seonghoon-Oh <su5785@naver.com>
2022-02-14 16:28:00 +0900

    feat: Add getUserPostings request handler

--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,12 @@
 const express = require("express");
 
-const { validateToken } = require("../middlewares/userValidation");
 const userController = require("../controllers/userController");
+const { validateToken } = require("../middlewares/userValidation");
+const { validateId } = require("../middlewares/objectIdValidation");
 
 const router = express.Router();
 
-router.get("/:id/maps", validateToken, userController.getUserMaps);
+router.get("/:id/maps", validateId, validateToken, userController.getUserMaps);
+router.get("/:id/postings", validateId, userController.getUserPostings);
 
 module.exports = router;
