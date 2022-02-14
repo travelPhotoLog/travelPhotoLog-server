cae7af903b6ba3026ef8e97e00756a3b7501b202
seonghoon-Oh <su5785@naver.com>
2022-02-14 20:51:23 +0900

    feat: Add deletePosting request handler

--- controllers/postingController.js
+++ controllers/postingController.js
@@ -129,7 +129,31 @@ const createPosting = async (req, res, next) => {
   }
 };
 
+const deletePosting = async (req, res, next) => {
+  const { id: postingId } = req.params;
+  const userId = req.query.user;
+
+  try {
+    await Promise.all([
+      User.updateOne({ _id: userId }, { $pull: { myPostings: postingId } }),
+      Posting.deleteOne({ _id: postingId }),
+    ]);
+
+    res.json({
+      result: "ok",
+    });
+  } catch {
+    res.json({
+      error: {
+        message: ERROR_MESSAGE.SERVER_ERROR,
+        code: 500,
+      },
+    });
+  }
+};
+
 exports.getPostings = getPostings;
 exports.getPostingDetail = getPostingDetail;
 exports.searchPostings = searchPostings;
 exports.createPosting = createPosting;
+exports.deletePosting = deletePosting;
